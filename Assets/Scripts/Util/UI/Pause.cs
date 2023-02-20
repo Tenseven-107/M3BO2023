4b9c095d5e377dc6cb1b61079e8d9f5d564f92ab
Tenseven-107 <78176691+Tenseven-107@users.noreply.github.com>
2023-02-20 22:17:51 +0100

    Updated the pause screen

--- Assets/Scripts/Util/UI/Pause.cs
+++ Assets/Scripts/Util/UI/Pause.cs
@@ -1,28 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
+    public float unpause_time = 4;
+
     GameObject state;
+    TextMeshProUGUI text;
     GameState game_state;
 
-    void Start()
+
+    private void Start()
     {
         Time.timeScale = 0.0f;
 
         state = GameObject.FindGameObjectWithTag("GameState");
         game_state = state.GetComponent<GameState>();
+
+        text = gameObject.transform.Find("Canvas/Text").gameObject.GetComponent<TextMeshProUGUI>();
     }
 
     private void Update()
     {
         if (Input.GetKeyDown("escape"))
         {
-            unpause();
+            StartCoroutine(WaitAndUnpause());
         }
     }
+
+
+    IEnumerator WaitAndUnpause()
+    {
+        float time = unpause_time;
+        while (time > 0.1)
+        {
+            time -= 0.1f * Time.fixedDeltaTime;
+            int time_int = (int)time;
+            text.text = time_int.ToString();
+
+            yield return null;
+        }
+        unpause();
+    }
+
 
     void unpause()
     {
