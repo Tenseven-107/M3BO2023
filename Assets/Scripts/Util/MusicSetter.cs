f738ebb8749b15d164475466f314f3740a560fa8
Tenseven-107 <78176691+Tenseven-107@users.noreply.github.com>
2023-04-04 00:18:07 +0200

    Added a music player

---
+++ Assets/Scripts/Util/MusicSetter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSetter : MonoBehaviour
+{
+    public int song = 0;
+
+    void Start()
+    {
+        GameObject music_player = GameObject.FindGameObjectWithTag("MusicPlayer");
+        MusicPlayer mp = music_player.GetComponent<MusicPlayer>();
+
+        mp.setSong(song);
+    }
+}
--- Assets/Scripts/Util/SFX_utils/MusicPlayer.cs
+++ Assets/Scripts/Util/SFX_utils/MusicPlayer.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AudioClip[] music_clips;
+    AudioSource music_source;
+
+
     void Start()
     {
-        
+        music_source = gameObject.AddComponent<AudioSource>();
+        music_source.loop = true;
+
+        if (gameObject.tag != "MusicPlayer") gameObject.tag = "MusicPlayer";
+
+        checkDoubles();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void checkDoubles()
     {
-        
+        GameObject[] music_players = GameObject.FindGameObjectsWithTag("MusicPlayer");
+
+        if (music_players.Length > 1) Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public void setSong(int song)
+    {
+        if (music_clips[song] != null && music_source.clip != music_clips[song]) music_source.clip = music_clips[song];
+        else return;
+
+        music_source.Play();
     }
 }
